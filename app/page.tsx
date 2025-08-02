c552eae379ff0fefd76370231f14d3294aee3d5e
Tharun Jatoth <venugopal.vallapu@gmail.com>
2025-08-02 11:10:58 +0530

    new 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,18 @@
 "use client";
 import React from "react";
-import Header from "@/components/Header";
-import Hero from "@/components/hero";
-import About from "@/components/About";
-import Experience from "@/components/Experience";
-import Skills from "@/components/Skills";
-import Projects from "@/components/Projects";
-import Education from "@/components/Education";
-import Footer from "@/components/Footer";
+import Header from "@/app/components/Header";
+import Hero from "@/app/components/hero";
+import About from "@/app/components/About";
+import Experience from "@/app/components/Experience";
+import Skills from "@/app/components/Skills";
+import Projects from "@/app/components/Projects";
+import Education from "@/app/components/Education";
+import Footer from "@/app/components/Footer";
 
 export default function Home() {
   return (
-    <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+    <div className="container mx-auto px-4 sm:px-6 lg:px-8 mt-16">
       <Hero />
-      {/* Main Content Sections */}
       <Header />
       <About />
       <Experience />
